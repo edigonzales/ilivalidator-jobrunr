acfb288e060526c8012af43b78c7be1159199757
Stefan Ziegler <edi.gonzales@gmail.com>
2021-04-03 21:08:00 +0200

    initial commit

---
+++ src/main/java/ch/so/agi/ilivalidator/IlivalidatorJobrunrApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi.ilivalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlivalidatorJobrunrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlivalidatorJobrunrApplication.class, args);
+    }
+
+}
