b2ef260771ccd241ab8a48088ff50bc4da3ab702
Stefan Ziegler <edi.gonzales@gmail.com>
2021-04-04 18:21:31 +0200

    in-memory Beispiel

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ilivalidator-jobrunr
+
+## Todo
+- PostgreSQL provider
+- Können zwei identisch konfigurierte Container laufen? Oder sind dann beides Master, was zu Problemen führt?
---
+++ src/main/java/ch/so/agi/ilivalidator/JobController.java
@@ -0,0 +1,43 @@
+package ch.so.agi.ilivalidator;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import org.jobrunr.scheduling.JobScheduler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JobController {
+    
+    @Autowired
+    private JobScheduler jobScheduler;
+
+    @Autowired
+    private SampleJobService sampleJobService;
+
+    @GetMapping("/run-job")
+    public String runJob(
+            @RequestParam(value = "name", defaultValue = "Hello World") String name) {
+
+        jobScheduler.enqueue(() -> sampleJobService.execute(name));
+        return "Job is enqueued.";
+
+    }
+
+    @GetMapping("/schedule-job")
+    public String scheduleJob(
+            @RequestParam(value = "name", defaultValue = "Hello World") String name,
+            @RequestParam(value = "when", defaultValue = "PT3H") String when) {
+
+        jobScheduler.schedule(() ->
+                        sampleJobService.execute(name),
+                Instant.now().plus(Duration.parse(when))
+        );
+
+        return "Job is scheduled.";
+    }
+
+}
